e765c790df8b1e4bbc1c3d1a72f9a025768b5a1b
cheese-cake-cookie <my.cheesecake.cookie@gmail.com>
2021-01-02 22:50:19 +0900

    마감일, deadline alert 추가

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 (() => {
+    const todoLayout = document.createElement('div');
+    let deadlineTimeout = null;
 	let todos = [];
 	let selectedTodo = null;
 
@@ -6,41 +8,117 @@
 		return localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')): [];
 	};
 
+	const setTodoList = () => {
+		return localStorage.setItem('todos', JSON.stringify(todos));
+    }
+    
+    const showDeadlineTodosDialog = () => {
+        clearTimeout(deadlineTimeout);
+        deadlineTimeout = null;
+        document.querySelector('.dialog').style.opacity = 1;
+        console.log('dialog check');
+        
+        deadlineTimeout = setTimeout(() => {
+            showDeadlineTodosDialog();
+        }, 5000)
+    };
+
+    const closeDialog = () => {
+        const dialog = document.querySelector('.dialog');
+
+        if (dialog.querySelector('input[name="show_after"]').checked) {
+            clearTimeout(deadlineTimeout);
+            deadlineTimeout = null;
+
+            deadlineTimeout = setTimeout(() => {
+                showDeadlineTodosDialog();
+            }, 1 * 60 * 1000);
+        }
+
+        dialog.style.opacity = 0;
+    }
+
+    const drawDeadlineTodosDialog = (el) => {
+        const deadlineApproachTodos = todos.filter(todo => todo.deadline && todo.status != 'DONE' && todo.until - new Date() <= 60 * 60 * 24 * 1000);
+        
+        if (! todos.length || ! deadlineApproachTodos.length) return;
+        
+        const fragment = document.createDocumentFragment();
+        const dialog = document.createElement('div');
+        const label = document.createElement('label');
+        const button = document.createElement('button');
+
+        dialog.setAttribute('class', 'dialog')
+        dialog.textContent = '하루밖에 남지 않았어요!';
+
+        deadlineApproachTodos.forEach(todo => {
+            const title = document.createElement('p');
+            title.setAttribute('class', 'title');
+            title.innerHTML = todo.title;
+            fragment.append(title);
+        });
+
+        label.innerHTML = `<input type="checkbox" name="show_after"> 1분 후 다시 보기`
+
+        button.textContent = '닫기';
+        button.addEventListener('click', (e) => {
+            closeDialog()
+        });
+
+        fragment.append(label, button);
+
+        dialog.append(fragment);
+        el.append(dialog);
+
+        deadlineTimeout = setTimeout(() => {
+            showDeadlineTodosDialog();
+        }, 1000);
+    }
+
 	const editTodo = (todo) => {
 		const todoList = document.querySelector('.todo');
-		// @TODO validation
+		let deadline = null;
+        let currentTime = +new Date();
+        
+        for (const until of todoList.querySelectorAll('.deadline input[name="deadline"]')) {
+            if (until.value && until.checked) deadline = until.value;
+            continue;
+        }
 
 		if (todo == null) {
 			const todo = {
-				id: null,
 				title: todoList.querySelector('.title').value,
-				description: todoList.querySelector('.description').value,
-				until: null,
+                description: todoList.querySelector('.description').value,
+                from: currentTime,
+                until: currentTime + (deadline * 60 * 60 * 24 * 1000),
+                deadline: deadline,
 				status: 'TODO',
-				seq: 0
 			};
 
 			todos.unshift(todo);
 		} else {
 			const todo = selectedTodo;
-
 			todo.title = todoList.querySelector('.title').value;
-			todo.description = todoList.querySelector('.description').value;
-			todos.splice(todos.indexOf(todo), 1, todo);
-
+            todo.description = todoList.querySelector('.description').value;
+            todo.until = deadline ? todo.from + (deadline * 60 * 60 * 24 * 1000) : null;
+            todo.deadline = deadline;
+            
+            todos.splice(todos.indexOf(todo), 1, todo);
 			selectedTodo = null;
 		}
 
-		localStorage.setItem('todos', JSON.stringify(todos));
 		todoList.querySelector('.todo-list').remove();
 		drawTodoList(todoLayout, todos);
+		setTodoList();
 		clearInput();
 	};
 
 	const deleteTodo = (el, todo) => {
+		const todoList = document.querySelector('.todo');
+
 		el.remove();
 		todos.splice(todos.indexOf(todo), 1);
-		console.log(todos);
+		setTodoList();
 	}
 
 	const setStatus = (el, todo) => {
@@ -54,7 +132,7 @@
 			todo.status = 'TODO';
 		}
 
-		console.log(todos);
+		setTodoList();
 	}
 
 	// todo list를 그림
@@ -82,16 +160,26 @@
 		contentInput.setAttribute('class', 'description');
 		contentInput.setAttribute('type', 'text');
 		contentInput.setAttribute('placeholder', '내용을 입력하세요');
-		
-		const button = document.createElement('button');
-		button.textContent = '입력';
-		button.addEventListener('click', (e) => editTodo(selectedTodo));
+
+        const until = document.createElement('div');
+        until.setAttribute('class', 'deadline');
+		until.innerHTML = `
+            <label><input type="radio" name="deadline" value="">설정안함</label>
+			<label><input type="radio" name="deadline" value="1">1일 후</label>
+			<label><input type="radio" name="deadline" value="7">7일 후</label>
+			<label><input type="radio" name="deadline" value="30">30일 후</label>
+        `;
+        
+        const buttonSave = document.createElement('button');
+        buttonSave.setAttribute('class', 'save-btn');
+		buttonSave.textContent = '입력';
+		buttonSave.addEventListener('click', (e) => editTodo(selectedTodo));
 
 		const buttonCancel = document.createElement('button');
 		buttonCancel.textContent = '취소';
 		buttonCancel.addEventListener('click', (e) => clearInput());
 
-		fragment.append(titleInput, contentInput, button, buttonCancel);
+		fragment.append(titleInput, contentInput, until, buttonSave, buttonCancel);
 		el.append(fragment);
 	}
 
@@ -100,6 +188,7 @@
 		const li = document.createElement('li');
 		const content = document.createElement('div');
 		const status = document.createElement('span');
+		const until = document.createElement('span');
 		const button = document.createElement('button');
 
 		li.setAttribute('class', 'item');
@@ -107,15 +196,25 @@
 		content.setAttribute('class', 'content');
 		content.addEventListener('click', (e) => {
 			document.querySelector('.todo > .title').value = todo.title;
-			document.querySelector('.todo > .description').value = todo.description;
+            document.querySelector('.todo > .description').value = todo.description;
+            for (const until of document.querySelectorAll('.todo > .deadline input[name="deadline"]')) {
+                if (until.value  == todo.deadline) until.checked = true;
+                continue;
+            }
+
 			selectedTodo = todo;
 		});
 		content.innerHTML = `
 			<h1 class="title">${todo.title}</h1>
 			<p class="description">${todo.description}</p>
-		`;
+        `;
+        
+        if (todo.deadline) {
+            const until = new Date(todo.until);
+            content.innerHTML += `<span class="until">${until.getMonth() + 1}월 ${until.getDate()}일까지</span>`;
+        }
 
-		status.setAttribute('class', 'status ready');
+		status.setAttribute('class', `status ${todo.status == 'TODO' ? 'ready' : 'done'}`);
 		status.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z"/></svg>`;
 		status.addEventListener('click', (e) => setStatus(li, todo));
 
@@ -123,7 +222,7 @@
 		button.textContent = '삭제';
 		button.addEventListener('click', (e) => deleteTodo(li, todo));
 
-		li.append(content, status, button);
+		li.append(content, status, until, button);
 		
 		return fragment.appendChild(li);
 	}
@@ -132,16 +231,18 @@
 		const todoList = document.querySelector('.todo');
 
 		todoList.querySelector('.title').value = '';
-		todoList.querySelector('.description').value = '';
+        todoList.querySelector('.description').value = '';
+        for (const until of document.querySelectorAll('.todo > .deadline input[name="deadline"]')) {
+            until.checked = false;
+        }
 	}
 
-	todos = getTodoList();
-	
-	const todoLayout = document.createElement('div');
-	todoLayout.setAttribute('class', 'todo');
-
+    todos = getTodoList();
+    console.log(todos);
+    
+    todoLayout.setAttribute('class', 'todo');
 	drawInputForm(todoLayout);
-	drawTodoList(todoLayout, todos);
-
+    drawTodoList(todoLayout, todos);
+    drawDeadlineTodosDialog(todoLayout);
 	document.body.append(todoLayout);
 })();
